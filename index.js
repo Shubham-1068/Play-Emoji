35ac0f6786730f99d7ba5424fe5419089382d63f
SHUBHAM <shubhamraj1068@gmail.com>
2025-01-22 03:55:48 +0530

    added files and initialized repo

---
+++ index.js
@@ -0,0 +1,34 @@
+import data from "./dataSet.js";
+
+let dataSet = data;
+
+function textToEmoji(text, extaData) {
+
+    if(extaData){
+        dataSet = {...dataSet, ...extaData};
+    }
+
+    text = text.toLowerCase();
+    text+=" "
+    let result = "";
+    let tmp = "";
+    for (let i = 0; i < text.length; i++) {
+        let letter = text[i];
+        if(letter != " "){
+            tmp += letter;
+        } else{
+            tmp = tmp.toLowerCase();
+            if(dataSet[tmp]){
+                result+=`${dataSet[tmp]}  `;
+            } else{
+                result+=`${tmp} `;
+            }
+            tmp="";
+        }
+    }
+
+    return result;
+        
+}
+
+export default textToEmoji;
